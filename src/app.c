e4bfc5a17dd428355bf592bd065f361cc54f06e9
Timo Schwierzeck <timo@schwierzeck.com>
2017-10-11 13:34:37 +0200

    app logic

--- src/app.c
+++ src/app.c
@@ -88,99 +88,107 @@
 #define RESET_BUTTON_INDEX_X 0
 #define SEND_BUTTON_INDEX_X 9
 
+#define SELECTIONS_PER_ROW 2
+#define DEFAULT_SELECTION 0
+
 // variables
 
 // colors    wht red grn blu amb wht cya mag yel wht
-u8 red[] = { 63, 63,  0,  0, 63, 63,  0, 63, 63, 63 };
-u8 grn[] = { 63,  0, 63,  0, 31, 63, 63,  0, 63, 63 };
-u8 blu[] = { 63,  0,  0, 63,  0, 63, 63, 63,  0, 63 };
+u8 red[] = {63, 63, 0, 0, 63, 63, 0, 63, 63, 63};
+u8 grn[] = {63, 0, 63, 0, 31, 63, 63, 0, 63, 63};
+u8 blu[] = {63, 0, 0, 63, 0, 63, 63, 63, 0, 63};
 
 u8 colorMapRed[ROWS_COUNT * BUTTONS_PER_ROW];
 u8 colorMapGrn[ROWS_COUNT * BUTTONS_PER_ROW];
 u8 colorMapBlu[ROWS_COUNT * BUTTONS_PER_ROW];
 
-u8 preSelectedIndexX[PAD_ROWS_COUNT];
-u8 selectedIndexX[PAD_ROWS_COUNT];
+u8 preSelectedIndexX[ROWS_COUNT][SELECTIONS_PER_ROW];
+u8 selectedIndexX[ROWS_COUNT][SELECTIONS_PER_ROW];
 
 void app_surface_event(u8 type, u8 index, u8 value)
 {
 	switch (type)
 	{
-		case  TYPEPAD:
+	case TYPEPAD:
+	{
+		// bottom row
+		if (index < BOTTOM_BUTTONS_COUNT)
 		{
-			// bottom row
-			if (index < BOTTOM_BUTTONS_COUNT)
+			if (index == SEND_ALL_BUTTON_INDEX)
 			{
-				if (index == SEND_ALL_BUTTON_INDEX)
+				if (value == 0)
+					break;
+
+				for (int indexY = BOTTOM_ROWS_COUNT; indexY < BOTTOM_ROWS_COUNT + SELECT_ROWS_COUNT; indexY++)
 				{
-					for (int indexY = BOTTOM_ROWS_COUNT; indexY < BOTTOM_ROWS_COUNT + SELECT_ROWS_COUNT; indexY++)
-					{
-						u8 newSelectedIndexX = preSelectedIndexX[indexY];
+					u8 newSelectedIndexX = preSelectedIndexX[indexY][DEFAULT_SELECTION];
+					if (newSelectedIndexX == 0)
+						continue;
 
-                                                if ( newSelectedIndexX == 0 )
-                                                        continue;
-
-                                                app_set_pre_selected_index(0, indexY);
-                                                app_set_selected_index(newSelectedIndexX, indexY);
-                                                break;
-					}
+					app_set_pre_selected_index(0, indexY);
+					app_set_selected_index(newSelectedIndexX, indexY);
 				}
 			}
-
-			// select rows
-			else if (index < BOTTOM_BUTTONS_COUNT + SELECT_BUTTONS_COUNT)
-			{
-				u8 indexX = index % BUTTONS_PER_ROW;
-				u8 indexY = (index - indexX) / BUTTONS_PER_ROW;
-
-				switch(indexX)
-				{
-					case RESET_BUTTON_INDEX_X: ;
-						if (value == 0)
-							break;
-						app_set_pre_selected_index(0, indexY);
-						app_set_selected_index(0, indexY);
-						break;
-
-					case SEND_BUTTON_INDEX_X: ;
-						if ( value == 0 )
-							break;
-
-						u8 newSelectedIndexX = preSelectedIndexX[indexY];
-
-						if ( newSelectedIndexX == 0 )
-							break;
-
-						app_set_pre_selected_index(0, indexY);
-						app_set_selected_index(newSelectedIndexX, indexY);
-						break;
-
-					default: ;
-						if ( indexX == selectedIndexX[indexY] )
-							break;
-
-						float intensity = value != 0 ? ACTIVE : INACTIVE;
-						hal_plot_led(TYPEPAD, index, colorMapRed[index] * intensity, colorMapGrn[index] * intensity, colorMapBlu[index] * intensity );
-						if (value != 0)
-							app_set_pre_selected_index(indexX, indexY);
-						break;
-				}
-			}
-
-			// top row
-			else if (index < BOTTOM_BUTTONS_COUNT + SELECT_BUTTONS_COUNT + TOP_BUTTONS_COUNT)
-			{
-				// ignore index 90 and 99
-			}
 		}
-		break;
 
-		case TYPESETUP:
+		// select rows
+		else if (index < BOTTOM_BUTTONS_COUNT + SELECT_BUTTONS_COUNT)
 		{
-			// example - light the setup LED
-			hal_plot_led(TYPESETUP, 0, value, value, value);
+			u8 indexX = index % BUTTONS_PER_ROW;
+			u8 indexY = (index - indexX) / BUTTONS_PER_ROW;
+
+			switch (indexX)
+			{
+			case RESET_BUTTON_INDEX_X:;
+				if (value == 0)
+					break;
+				app_set_pre_selected_index(0, indexY);
+				app_set_selected_index(0, indexY);
+				break;
+
+			case SEND_BUTTON_INDEX_X:;
+				if (value == 0)
+					break;
+
+				u8 newSelectedIndexX = preSelectedIndexX[indexY][DEFAULT_SELECTION];
+
+				if (newSelectedIndexX == 0)
+					break;
+
+				app_set_pre_selected_index(0, indexY);
+				app_set_selected_index(newSelectedIndexX, indexY);
+				break;
+
+			default:
+				// if already selected break
+				if (indexX == selectedIndexX[indexY][DEFAULT_SELECTION])
+					break;
+
+				// light up button while pressing
+				float intensity = value != 0 ? ACTIVE : INACTIVE;
+				hal_plot_led(TYPEPAD, index, colorMapRed[index] * intensity, colorMapGrn[index] * intensity, colorMapBlu[index] * intensity);
+
+				// set pre selected index
+				if (value != 0)
+					app_set_pre_selected_index(indexX, indexY);
+				break;
+			}
 		}
-		break;
+
+		// top row
+		else if (index < BOTTOM_BUTTONS_COUNT + SELECT_BUTTONS_COUNT + TOP_BUTTONS_COUNT)
+		{
+			// ignore index 90 and 99
+		}
+	}
+	break;
+
+	case TYPESETUP:
+	{
+		// example - light the setup LED
+		hal_plot_led(TYPESETUP, 0, value, value, value);
+	}
+	break;
 	}
 }
 
@@ -203,23 +211,21 @@ void app_midi_event(u8 port, u8 status, u8 d1, u8 d2)
 
 //______________________________________________________________________________
 
-void app_sysex_event(u8 port, u8 * data, u16 count)
+void app_sysex_event(u8 port, u8 *data, u16 count)
 {
-	// example - respond to UDI messages?
 }
 
 //______________________________________________________________________________
 
 void app_aftertouch_event(u8 index, u8 value)
 {
-
 }
 
 //______________________________________________________________________________
 
 void app_cable_event(u8 type, u8 value)
 {
-    // example - light the Setup LED to indicate cable connections
+	// example - light the Setup LED to indicate cable connections
 	if (type == MIDI_IN_CABLE)
 	{
 		hal_plot_led(TYPESETUP, 0, 0, value, 0); // green
@@ -232,10 +238,9 @@ void app_cable_event(u8 type, u8 value)
 
 //______________________________________________________________________________
 
-
 void app_timer_event()
 {
-	// example - send MIDI clock at 125bpm
+// example - send MIDI clock at 125bpm
 #define TICK_MS_ON 0
 #define TICK_MS_OFF 500
 #define TICK_RESET 1000
@@ -245,9 +250,9 @@ void app_timer_event()
 	if (ms == TICK_MS_ON || ms == TICK_MS_OFF)
 	{
 		float intensity = ms == TICK_MS_ON ? ACTIVE : INACTIVE;
-		for ( int indexY = 1; indexY <= PAD_ROWS_COUNT; indexY++ )
+		for (int indexY = 1; indexY <= PAD_ROWS_COUNT; indexY++)
 		{
-			u8 indexX = preSelectedIndexX[indexY];
+			u8 indexX = preSelectedIndexX[indexY][DEFAULT_SELECTION];
 			if (indexX == 0)
 				continue;
 			u8 index = indexY * BUTTONS_PER_ROW + indexX;
@@ -256,15 +261,20 @@ void app_timer_event()
 	}
 
 	ms++;
-	if( ms >= TICK_RESET )
+	if (ms >= TICK_RESET)
 		ms = 0;
-
 }
 
 //______________________________________________________________________________
 
 void app_init()
 {
+	for (int indexY = 0; indexY < ROWS_COUNT; indexY++)
+	{
+		selectedIndexX[indexY][DEFAULT_SELECTION] = 0;
+		preSelectedIndexX[indexY][DEFAULT_SELECTION] = 0;
+	}
+
 	app_init_color_map();
 	app_init_leds();
 }
@@ -272,12 +282,10 @@ void app_init()
 void app_init_color_map()
 {
 	// init color map
-	for (int index = 0; index < ( ROWS_COUNT * BUTTONS_PER_ROW ); index++)
+	for (int index = 0; index < (ROWS_COUNT * BUTTONS_PER_ROW); index++)
 	{
 		u8 indexX = index % BUTTONS_PER_ROW;
 		u8 indexY = (index - indexX) / BUTTONS_PER_ROW;
-
-		hal_plot_led(TYPEPAD, index, 63, 63, 63);
 
 		if (index < BOTTOM_BUTTONS_COUNT)
 		{
@@ -290,14 +298,14 @@ void app_init_color_map()
 			colorMapRed[index] = red[indexX];
 			colorMapGrn[index] = grn[indexX];
 			colorMapBlu[index] = blu[indexX];
-                }
+		}
 		else if (index < BOTTOM_BUTTONS_COUNT + COLOR_BUTTONS_COUNT + GOBO_BUTTONS_COUNT)
 		{
-                        if (indexX != RESET_BUTTON_INDEX_X && indexX != SEND_BUTTON_INDEX_X)
+			if (indexX != RESET_BUTTON_INDEX_X && indexX != SEND_BUTTON_INDEX_X)
 			{
 				colorMapRed[index] = 0;
-                        	colorMapGrn[index] = 63;
-                        	colorMapBlu[index] = 63;
+				colorMapGrn[index] = 63;
+				colorMapBlu[index] = 63;
 			}
 			else
 			{
@@ -305,9 +313,9 @@ void app_init_color_map()
 				colorMapGrn[index] = 63;
 				colorMapBlu[index] = 63;
 			}
-                }
+		}
 		else if (index < BOTTOM_BUTTONS_COUNT + COLOR_BUTTONS_COUNT + GOBO_BUTTONS_COUNT + FOCUS_BUTTONS_COUNT)
-                {
+		{
 			if (indexX != RESET_BUTTON_INDEX_X && indexX != SEND_BUTTON_INDEX_X)
 			{
 				colorMapRed[index] = 0;
@@ -322,7 +330,7 @@ void app_init_color_map()
 			}
 		}
 		else if (index < BOTTOM_BUTTONS_COUNT + COLOR_BUTTONS_COUNT + GOBO_BUTTONS_COUNT + FOCUS_BUTTONS_COUNT + POSITION_BUTTONS_COUNT)
-                {
+		{
 			if (indexX != RESET_BUTTON_INDEX_X && indexX != SEND_BUTTON_INDEX_X)
 			{
 				colorMapRed[index] = 63;
@@ -336,7 +344,7 @@ void app_init_color_map()
 				colorMapBlu[index] = 63;
 			}
 		}
-		else if (index < BOTTOM_BUTTONS_COUNT + SELECT_BUTTONS_COUNT  + TOP_BUTTONS_COUNT)
+		else if (index < BOTTOM_BUTTONS_COUNT + SELECT_BUTTONS_COUNT + TOP_BUTTONS_COUNT)
 		{
 			colorMapRed[index] = 0;
 			colorMapGrn[index] = 0;
@@ -347,7 +355,7 @@ void app_init_color_map()
 
 void app_init_leds()
 {
-	for (int index = 0; index < ( ROWS_COUNT * BUTTONS_PER_ROW ); index++)
+	for (int index = 0; index < (ROWS_COUNT * BUTTONS_PER_ROW); index++)
 	{
 		hal_plot_led(TYPEPAD, index, colorMapRed[index] * INACTIVE, colorMapGrn[index] * INACTIVE, colorMapBlu[index] * INACTIVE);
 	}
@@ -356,7 +364,7 @@ void app_init_leds()
 void app_set_selected_index(u8 indexX, u8 indexY)
 {
 	// logic
-	u8 oldSelectedIndexX = selectedIndexX[indexY];
+	u8 oldSelectedIndexX = selectedIndexX[indexY][DEFAULT_SELECTION];
 	u8 oldSelectedIndex = indexY * BUTTONS_PER_ROW + oldSelectedIndexX;
 
 	u8 newSelectedIndexX = indexX;
@@ -365,7 +373,7 @@ void app_set_selected_index(u8 indexX, u8 indexY)
 	u8 sendButtonIndexX = SEND_BUTTON_INDEX_X;
 	u8 sendButtonIndex = indexY * BUTTONS_PER_ROW + sendButtonIndexX;
 
-	selectedIndexX[indexY] = newSelectedIndexX;
+	selectedIndexX[indexY][DEFAULT_SELECTION] = newSelectedIndexX;
 
 	// visualization
 	if (oldSelectedIndex != 0)
@@ -388,7 +396,7 @@ void app_set_selected_index(u8 indexX, u8 indexY)
 void app_set_pre_selected_index(u8 indexX, u8 indexY)
 {
 	// logic
-	u8 oldPreSelectedIndexX = preSelectedIndexX[indexY];
+	u8 oldPreSelectedIndexX = preSelectedIndexX[indexY][DEFAULT_SELECTION];
 	u8 oldPreSelectedIndex = indexY * BUTTONS_PER_ROW + oldPreSelectedIndexX;
 
 	u8 newPreSelectedIndexX = indexX;
@@ -397,9 +405,9 @@ void app_set_pre_selected_index(u8 indexX, u8 indexY)
 	if (oldPreSelectedIndexX == newPreSelectedIndexX)
 		return;
 
-	preSelectedIndexX[indexY] = newPreSelectedIndexX;
+	preSelectedIndexX[indexY][DEFAULT_SELECTION] = newPreSelectedIndexX;
 
 	// visualization
-	if ( oldPreSelectedIndexX != 0 && oldPreSelectedIndexX != selectedIndexX[indexY] )
+	if (oldPreSelectedIndexX != 0 && oldPreSelectedIndexX != selectedIndexX[indexY][DEFAULT_SELECTION])
 		hal_plot_led(TYPEPAD, oldPreSelectedIndex, colorMapRed[oldPreSelectedIndex] * INACTIVE, colorMapGrn[oldPreSelectedIndex] * INACTIVE, colorMapBlu[oldPreSelectedIndex] * INACTIVE);
 }
